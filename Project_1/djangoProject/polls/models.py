981b5ee914bc754f0eb48610099b7e2fc96276e7
Bogdan <bogdan.trinc@student.upt.ro>
2022-07-21 12:48:02 +0300

    Final Version

--- Project_1/djangoProject/polls/models.py
+++ Project_1/djangoProject/polls/models.py
@@ -12,6 +12,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
@@ -26,12 +31,15 @@ class Choice(models.Model):
         return self.choice_text
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
