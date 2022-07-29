53afd34963713831351380f68d0d93ef7a78d011
Bogdan <bogdan.trinc@student.upt.ro>
2022-07-29 16:57:41 +0300

    custom-migrations-and-tests

--- Project_2/djangoProject/cars/tests.py
+++ Project_2/djangoProject/cars/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from cars.models import Car
+
+
+class CarIndexViewTests(TestCase):
+    def test_no_cars(self):
+        """
+        If no cars exist, an apropriate message is displayed.
+        """
+        Car.objects.all().delete()
+        response = self.client.get(reverse('cars:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No cars are available.")
+        self.assertQuerysetEqual(response.context['car_list'], [])
+
 
 # Create your tests here.
