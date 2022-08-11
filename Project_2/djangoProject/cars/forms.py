7f00873b16c85c01a1cee13c41af4dcd591caef1
Bogdan <bogdan.trinc@student.upt.ro>
2022-08-11 09:24:21 +0300

    register-page-endpoint

--- Project_2/djangoProject/cars/forms.py
+++ Project_2/djangoProject/cars/forms.py
@@ -5,18 +5,31 @@ from django.contrib.auth.models import User
 
 
 class RegisterUser(UserCreationForm):
-	first_name = forms.CharField(max_length=12, min_length=4, required=True,
-								 widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'John'}))
-	last_name = forms.CharField(max_length=12, min_length=4, required=True,
-								widget=(forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Doe'})))
-	address = forms.CharField(max_length=12, min_length=4,
-								widget=(forms.TextInput(attrs={'class': 'form-control'})))
-	email = forms.EmailField(max_length=50, widget=(forms.TextInput(attrs={'class': 'form-control',
-																		   'placeholder': 'john.doe@email.com'})))
-	password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-	password2 = forms.CharField(label='Password confirmation',
-								widget=forms.PasswordInput(attrs={'class': 'form-control'}),
-								help_text='Enter the same password as before, for verification.')
+	first_name = forms.CharField(
+		max_length=12, min_length=4, required=True,
+		widget=forms.TextInput(
+			attrs={'class': 'form-control', 'placeholder': 'John'}))
+	last_name = forms.CharField(
+		max_length=12, min_length=4, required=True,
+		widget=(forms.TextInput(
+			attrs={'class': 'form-control', 'placeholder': 'Doe'})))
+	address = forms.CharField(
+		max_length=12, min_length=4,
+		widget=(forms.TextInput(
+			attrs={'class': 'form-control'})))
+	email = forms.EmailField(
+		max_length=50,
+		widget=(forms.TextInput(
+			attrs={'class': 'form-control', 'placeholder': 'john.doe@email.com'})))
+	password1 = forms.CharField(
+		label='Password',
+		widget=forms.PasswordInput(
+			attrs={'class': 'form-control'}))
+	password2 = forms.CharField(
+		label='Password confirmation',
+		help_text='Enter the same password as before, for verification.',
+		widget=forms.PasswordInput(
+			attrs={'class': 'form-control'}))
 
 	class Meta:
 		model = User
